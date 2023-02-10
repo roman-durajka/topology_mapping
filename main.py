d25dba0ad2976ebf285c48a623a9439c1f085f7d
Roman Ďurajka <romo.durajka@gmail.com>
2023-02-10 18:01:37 +0100

    added DP extractor

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from extractors import MacExtractor, IPExtractor, DeviceExtractor
+from extractors import MacExtractor, IPExtractor, DeviceExtractor, DPExtractor
 from modules.clients import MariaDBClient
 
 
 def main():
     db_client = MariaDBClient()
-    mac_extractor = MacExtractor(db_client)
-    ip_extractor = IPExtractor(db_client)
     device_extractor = DeviceExtractor(db_client)
-
     devices = device_extractor.extract()
-    mac_relations = mac_extractor.extract()
-    ip_relations = ip_extractor.extract()
 
-    relations = f"{mac_relations}\n{ip_relations}"
+    mac_extractor = MacExtractor(db_client)
+    mac_relations = mac_extractor.extract()
+
+    ip_extractor = IPExtractor(db_client, mac_relations)
+    ip_relations = ip_extractor.extract()
+    #TODO: COMMAND LINE ARGUMENT TO FORCE CDP/LLDP ONLY ip_relations = entities.RelationsContainer()
+    dp_extractor = DPExtractor(db_client, ip_relations)
+    dp_relations = dp_extractor.extract()
+
+    relations = f"{dp_relations}"
 
     file_to_write = open("graph_example.dot", "w")
     file_to_write.write("graph G {\n")
