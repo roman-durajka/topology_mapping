7c529483c072d0600502b9d98c0f8f89c8452725
Roman Ďurajka <romo.durajka@gmail.com>
2023-10-21 17:08:47 +0200

    added new dump, edited old one, added entry db entry point for docker to load all dumps on build

--- dumps/data_model_dump_librenms.sql
+++ dumps/data_model_dump_librenms.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `devices`
 --
 
+CREATE DATABASE `librenms`;
+USE `librenms`;
+
+
 DROP TABLE IF EXISTS `devices`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
