0aff39686df6ab067b0f31d7838475ae72f703cc
Roman Ďurajka <romo.durajka@gmail.com>
2023-10-21 17:07:36 +0200

    changed db initialization for different objects, added TODO for data loader

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ import sys
 
 
 def main():
-    db_client = MariaDBClient()
-    device_extractor = DeviceExtractor(db_client)
+    topology_db_client = MariaDBClient("librenms")
+    device_extractor = DeviceExtractor(topology_db_client)
     devices = device_extractor.extract()
 
-    asset_map = asset_mapper.AssetMapper()
+    risk_mgmt_db_client = MariaDBClient("risk_management")
+
+    # TODO: integrate into UI when it's done
+    # data_load = DataLoader(risk_mgmt_db_client)
+    # data_load.load_assets()
+    # data_load.load_risks()
+
+    asset_map = asset_mapper.AssetMapper(risk_mgmt_db_client)
     asset_map.assign_risks_to_devices(devices)
 
     relations = entities.RelationsContainer()
     if len(sys.argv) > 1 and sys.argv[1] == "custom":  # uses custom algorithm if argument "custom" is specified
-        mac_extractor = MacExtractor(db_client, relations)
+        mac_extractor = MacExtractor(topology_db_client, relations)
         mac_relations = mac_extractor.extract()
 
-        ip_extractor = IPExtractor(db_client, mac_relations)
+        ip_extractor = IPExtractor(topology_db_client, mac_relations)
         relations = ip_extractor.extract()
     else:  # uses discovery protocols algorithm if nothing else is specified
-        dp_extractor = DPExtractor(db_client, relations)
+        dp_extractor = DPExtractor(topology_db_client, relations)
         relations = dp_extractor.extract()
 
     js_data_json = topology_generator.generate_js_data_json(devices, relations)
